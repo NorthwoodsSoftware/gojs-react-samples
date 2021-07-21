6d6c8336b45afd046c927bbb8f816881592ea3f6
Jon Hardy <jonathan.c.hardy@gmail.com>
2021-07-21 14:53:56 -0400

    add Robot and simulated input test

--- gojs-react-jest/src/App.test.tsx
+++ gojs-react-jest/src/App.test.tsx
@@ -1,11 +1,13 @@
 import * as go from "gojs";
 import { render } from '@testing-library/react';
 import App from './App';
+import { Robot } from './Robot';
 
 describe('ReactDiagram tests', () => {
   let diagram: go.Diagram | null = null;
+  let robot: Robot | null = null;
 
-  // initialize the Diagram prior to all tests
+  // initialize the Diagram and Robot prior to all tests
   beforeAll(() => {
     // use Jest's fake timers to ensure Diagram.delayInitialization is called in time
     jest.useFakeTimers();
@@ -13,6 +15,7 @@ describe('ReactDiagram tests', () => {
     jest.runOnlyPendingTimers();
     // grab the diagram via its div from the class name given to it
     diagram = (container.getElementsByClassName('diagram-component')[0] as any).goDiagram;
+    robot = new Robot(diagram);
   });
 
   afterAll(() => {
@@ -41,5 +44,25 @@ describe('ReactDiagram tests', () => {
 
     expect(diagram?.nodes.count).toBe(3);
     expect(diagram?.findNodeForKey(1)).toBeNull();
+  });
+
+  test('node copied with simulated input', () => {
+    const delta = diagram?.findNodeForKey(3) ?? null;
+    const loc = delta?.actualBounds.center ?? new go.Point();
+
+    // Simulate a mouse drag to move the Delta node:
+    const options = { control: true, alt: true };
+    robot?.mouseDown(loc.x, loc.y, 0, options);
+    robot?.mouseMove(loc.x + 80, loc.y + 50, 50, options);
+    robot?.mouseMove(loc.x + 20, loc.y + 100, 100, options);
+    robot?.mouseUp(loc.x + 20, loc.y + 100, 150, options);
+    // If successful, this will have made a copy of the "Delta" node below it.
+
+    const newloc = new go.Point(loc.x + 20, loc.y + 100);
+    const newnode = diagram?.findPartAt(newloc);
+
+    expect(newnode).not.toBe(delta);
+    expect(newnode?.key).toBe(4);
+    expect(newnode?.isSelected).toBeTruthy();
   });
 });
